f4c124023ffb0234d44f01747ba4770e8071909b
mikko <mikko>
2021-12-28 22:20:55 +0200

    wip implementing program 2

--- rust/wknd2/src/main.rs
+++ rust/wknd2/src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
+#![allow(dead_code)]
+
+mod vec3;
+use vec3::vec3::Vec3;
+use vec3::vec3::Ray3;
+use vec3::vec3::vec3;
+use vec3::vec3::lerp3;
+use vec3::vec3::unit_vector;
+
+fn test_ppm(){
+
     let nx = 200;
     let ny = 100;
     println!("P3\n{} {}\n255", nx, ny);
@@ -6,13 +16,25 @@ fn main() {
     let fny = ny as f32;
     for j in (0 .. ny).rev() {
         for i in 0 .. nx {
-            let r = (i as f32) / fnx;
-            let g = (j as f32) / fny;
-            let b : f32 = 0.2;
-            let ir = (255.99 * r) as i32;
-            let ig = (255.99 * g) as i32;
-            let ib = (255.99 * b) as i32;
+            let col = vec3((i as f32) / fnx, (j as f32) / fny, 0.2);
+            let ir = (255.99 * col.x) as i32;
+            let ig = (255.99 * col.y) as i32;
+            let ib = (255.99 * col.z) as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
+}
+
+fn color(r : Ray3) -> Vec3 {
+    let udir = unit_vector(r.direction());
+    let t = 0.5 * (udir.y + 1.0);
+    lerp3(Vec3{x:1.0,y:1.0,z:1.0}, Vec3{x:0.5, y:0.7, z:1.0}, t)
+}
+
+fn main() {
+    let a = Vec3{x:0.1, y:0.1, z:0.1};
+    let b = Vec3{x:1.0, y:1.0, z:1.0};
+    let _c = a + b;
+    //println!("{:?}",c );
+    test_ppm();
 }
---
+++ rust/wknd2/src/vec3.rs
@@ -0,0 +1,89 @@
+#![allow(dead_code)]
+pub mod vec3 {
+use std::ops::{Add, Sub, Mul};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Vec3 {
+    pub x : f32, pub y: f32, pub z:f32
+}
+impl Vec3 {
+    fn length(self) -> f32{
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+}
+
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, t:f32) -> Vec3 {
+            Vec3 {x: self.x * t , y: self.y * t, z: self.z * t}
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = f32;
+    fn mul(self, t:Vec3) -> f32 {
+            self.x * t.x + self.y * t.y + self.z * t.z
+    }
+}
+
+//impl Mul<Vec3> for f32{
+//    type Output = Vec3;
+//    fn mul(self, t:Vec3) -> Vec3 {
+//            Vec3{x : self * t.x, y: self * t.y, z:self * t.z
+//    }
+//}
+
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, other:Self) -> Self {
+        Self{
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z
+        }
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, other:Self) -> Self {
+        Self{
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z
+        }
+    }
+}
+
+
+pub fn vec3(x : f32, y:f32, z:f32)->Vec3 {
+    Vec3{x:x, y:y, z:z}
+}
+
+pub fn unit_vector(v:Vec3) -> Vec3{
+    let il = 1.0 / v.length();
+    v * il
+}
+
+pub fn lerp3(a:Vec3, b:Vec3, t : f32) -> Vec3{
+    let u = 1.0 - t;
+    (a * u) + (b * t)
+}
+
+//
+// Ray3
+//
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Ray3{
+    pub a : Vec3, pub b : Vec3
+}
+impl Ray3{
+    pub fn origin(&self) -> Vec3 {self.a} 
+    pub fn direction(&self) -> Vec3 {self.b} 
+    pub fn point_at_parameter(&self, t:f32) -> Vec3 {
+        self.a + (self.b * t)
+    }
+}
+
+}
